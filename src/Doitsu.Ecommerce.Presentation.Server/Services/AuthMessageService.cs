d8d9cc6dcee41aae4312b1d5748712bd7321769b
doitsu2014 <duc.tran@doitsu.tech>
2020-12-09 15:29:40 +0700

    Polly

--- src/Doitsu.Ecommerce.Presentation.Server/Services/AuthMessageService.cs
+++ src/Doitsu.Ecommerce.Presentation.Server/Services/AuthMessageService.cs
@@ -7,30 +7,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Polly;
 
 namespace Doitsu.Ecommerce.Presentation.Server.Services
 {
     public class AuthMessageService : IEmailSender
     {
+        private readonly ILogger<AuthMessageService> _logger;
         private readonly SmtpClientSettings _settings;
 
-        public AuthMessageService(IOptions<SmtpClientSettings> smtpClientSettingsOptions)
+        public AuthMessageService(IOptions<SmtpClientSettings> smtpClientSettingsOptions,
+            ILogger<AuthMessageService> logger)
         {
+            _logger = logger;
             _settings = smtpClientSettingsOptions.Value;
         }
 
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var client = new SmtpClient(_settings.Host, _settings.Port)
-            {
-                Credentials = new NetworkCredential(_settings.Mail, _settings.Password),
-                EnableSsl = _settings.EnableSsl
-            };
+            await Policy
+                .Handle<SmtpException>()
+                .RetryAsync(3,
+                    (ex, retryCount, ctx) => { _logger.LogError("AuthMessageService SendEmailAsync exception: ", ex); })
+                .ExecuteAsync(
+                    async () =>
+                    {
+                        var client = new SmtpClient(_settings.Host, _settings.Port)
+                        {
+                            Credentials = new NetworkCredential(_settings.Mail, _settings.Password),
+                            EnableSsl = _settings.EnableSsl
+                        };
 
-            await client.SendMailAsync(
-                new MailMessage(_settings.Mail, email, subject, htmlMessage) { IsBodyHtml = true }
-            );
+                        await client.SendMailAsync(
+                            new MailMessage(_settings.Mail, email, subject, htmlMessage) {IsBodyHtml = true}
+                        );
+                    }
+                );
         }
     }
 }
